57a45df96ab734857031676574b550917820f735
Arthur Teixeira <arthursony1@hotmail.com>
2023-02-06 16:21:36 -0300

    parte 5

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ function createPalette() {
   }
 }
 
-createPalette();
-
 function randomizeColor() {
   return `rgb(
     ${Math.floor(Math.random() * 256)}, 
@@ -16,14 +14,45 @@ function randomizeColor() {
     ${Math.floor(Math.random() * 256)})`;
 }
 
-console.log(randomizeColor());
+const SaveStorage = (localStorageColors) => {
+  const color = document.querySelectorAll('.color');
+  color[0].style.backgroundColor = 'black';
+  for (let i = 1; i < color.length; i += 1) {
+    color[i].style.backgroundColor = localStorageColors[i - 1];
+  }
+};
 
 function setColor() {
   const color = document.querySelectorAll('.color');
   color[0].style.backgroundColor = 'black';
+  const colors = [];
   for (let i = 1; i < color.length; i += 1) {
-    color[i].style.backgroundColor = randomizeColor();
+    const random = randomizeColor();
+    color[i].style.backgroundColor = random;
+    colors.push(random);
+  }
+  localStorage.setItem('colorPalette', JSON.stringify(colors));
+}
+
+function loadStorage() {
+  const localStorageColors = JSON.parse(localStorage.getItem('colorPalette'));
+  if (localStorageColors !== null) {
+    SaveStorage(localStorageColors);
+  } else {
+    setColor();
   }
 }
 
-setColor();
+const randomizeColorButton = () => {
+  const randomColorButton = document.createElement('button');
+  randomColorButton.setAttribute('id', 'button-random-color');
+  randomColorButton.innerText = 'Cores aleatórias';
+  randomColorButton.addEventListener('click', setColor);
+  const colorPalette = document.getElementById('color-palette');
+  colorPalette.appendChild(randomColorButton);
+};
+
+createPalette();
+randomizeColorButton();
+
+window.onload = loadStorage;
